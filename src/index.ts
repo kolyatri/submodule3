0ef4d9de82395795239363a4e17b1fa37cee6ccb
kolyatri <kolyatri@gmail.com>
2019-03-21 16:17:35 +0200

    added all typerom libraries+ made query

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+
+import "reflect-metadata";
+import {createConnection, getManager} from "typeorm";
+
+//export const Greeter = (name: string) => `Hello ${name}`;
+export const Greeter = async (manager: any) => {
+    //createConnection().then(async connection => { 
+    //    console.log("successfully connected");
+        let users = await manager.query("select * from freelotto.users limit 1");
+        console.log(users);
+  //  }).catch(error => console.log(error));      
+};
