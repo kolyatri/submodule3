af5ef99fba4086d82daf76ec0deb039dcdb98af1
kolyatri <kolyatri@gmail.com>
2019-03-22 11:54:59 +0200

    moved typeorm dependencies to dev dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,7 @@
 import "reflect-metadata";
 import {createConnection, getManager} from "typeorm";
 
-//export const Greeter = (name: string) => `Hello ${name}`;
-export const Greeter = async (manager: any) => {
-    //createConnection().then(async connection => { 
-    //    console.log("successfully connected");
-        let users = await manager.query("select * from freelotto.users limit 1");
-        console.log(users);
-  //  }).catch(error => console.log(error));      
+export const Greeter = async (manager: any) => { 
+  let users = await manager.query("select * from freelotto.users limit 1");
+  console.log(users);  
 };
