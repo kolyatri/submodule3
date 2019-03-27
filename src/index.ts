4d0b2ea225a8d40d47611a7a0dc6fa11d3d42b6e
kolyatri <kolyatri@gmail.com>
2019-03-27 10:47:53 +0200

    added verifyUserToken function

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,29 @@
 import "reflect-metadata";
 import {createConnection, getManager} from "typeorm";
 
-export const Greeter = async (manager: any) => { 
+/*export const Greeter = async (manager: any) => { 
   let users = await manager.query("select * from freelotto.users limit 1");
   console.log(users);  
-};
+};*/
+
+export class freelottoAuth {
+  //private currentToken:any;
+  private myCache:any;
+
+  contructor(cache:any ){
+    //this.currentToken = token;
+    this.myCache =  cache;
+  }
+
+  public verifyUserToken(token:string){
+    let user =  this.myCache.get(token);
+    if ( user == undefined ){
+      console.log("undefined");
+    }
+    else{
+      console.log(user);
+    }
+
+  }
+}
+
