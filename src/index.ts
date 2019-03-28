0c940932917de44806d6c611da97d00108d83103
kolyatri <kolyatri@gmail.com>
2019-03-28 13:13:14 +0200

    added verifyAdminToken, now both functions retrieve id of user of valid token

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 
 import "reflect-metadata";
 import {createConnection, getManager} from "typeorm";
-import { AnyTxtRecord } from "dns";
 
-
-export const Greeter = async (manager: any) => { 
-  let users = await manager.query("select * from freelotto.users limit 1");
-  console.log(users);  
-};
 
 export class FreelottoAuth {
   //private currentToken:any;
@@ -28,9 +22,9 @@ export class FreelottoAuth {
     if ( user == undefined ){  
      
       let users = await this.manager
-      .query(
-        "select * from freelotto.user_token where token = $1",[token]
-      );     
+        .query(
+          "select * from freelotto.user_token where token = $1",[token]
+        );     
 
       if(users.length > 0){          
         let user = users[0];
@@ -44,7 +38,7 @@ export class FreelottoAuth {
           let obj = { id: user.userid, type: "user"};
           this.myCache.set( token, obj, ttl );
 
-          return true;
+          return {id: user.userid};
         }
         else{
           //console.log("expired");
@@ -61,9 +55,54 @@ export class FreelottoAuth {
         return false;
 
       else 
-       return true;
+        return {id: user.userid};
     }
 
   }
+
+  public verifyAdminToken = async (token:string) => {    
+    let admin =  this.myCache.get(token); 
+    
+    if ( admin == undefined ){  
+     
+      let admins = await this.manager
+      .query(
+        "select * from freelotto.admin_token where token = $1",[token]
+      );     
+
+      if(admins.length > 0){          
+        let admin = admins[0];
+        let expiredTimestamp = new Date(admin.expired_timestamp).getTime();
+        let currentTimestamp = new Date().getTime();
+
+        if(expiredTimestamp > currentTimestamp){
+          //console.log("found in DB and inserted into cache");
+          let ttl = Math.floor((expiredTimestamp - currentTimestamp)/1000);      
+
+          let obj = { id: admin.userid, type: "admin"};
+          this.myCache.set( token, obj, ttl );
+
+          return {id: admin.userid};
+        }
+        else{
+          //console.log("expired");
+          return false;
+        }               
+      }
+      else{
+        //console.log("no token in DB");
+        return false;
+      }
+    }    
+    else{      
+      if(admin.type != "admin") 
+        return false;
+
+      else 
+        return {id: admin.userid};
+    }
+
+  }
+
 }
 
