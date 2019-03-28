ca595cdcd5633f073257aa90dc225ce32513c0e3
kolyatri <kolyatri@gmail.com>
2019-03-28 12:36:20 +0200

    creted class and added verifyUserToken method

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,69 @@
 
 import "reflect-metadata";
 import {createConnection, getManager} from "typeorm";
+import { AnyTxtRecord } from "dns";
 
-/*export const Greeter = async (manager: any) => { 
+
+export const Greeter = async (manager: any) => { 
   let users = await manager.query("select * from freelotto.users limit 1");
   console.log(users);  
-};*/
+};
 
-/*export class FreelottoAuth {
+export class FreelottoAuth {
   //private currentToken:any;
   private myCache:any;
+  private manager:any;
 
-  contructor(cache:any){   
-    this.myCache =  cache;
+  contructor(){      
+  }
+ 
+  public init(cache:any, manager:any){
+    this.myCache = cache;
+    this.manager = manager;
   }
 
-  public verifyUserToken(token:string){
-    let user =  this.myCache.get(token);
-    if ( user == undefined ){
-      console.log("undefined");
+  public verifyUserToken = async (token:string) => {    
+    let user =  this.myCache.get(token); 
+    
+    if ( user == undefined ){  
+     
+      let users = await this.manager
+      .query(
+        "select * from freelotto.user_token where token = $1",[token]
+      );     
+
+      if(users.length > 0){          
+        let user = users[0];
+        let expiredTimestamp = new Date(user.expired_timestamp).getTime();
+        let currentTimestamp = new Date().getTime();
+
+        if(expiredTimestamp > currentTimestamp){
+          //console.log("found in DB and inserted into cache");
+          let ttl = Math.floor((expiredTimestamp - currentTimestamp)/1000);      
+
+          let obj = { id: user.userid, type: "user"};
+          this.myCache.set( token, obj, ttl );
+
+          return true;
+        }
+        else{
+          //console.log("expired");
+          return false;
+        }               
+      }
+      else{
+        //console.log("no token in DB");
+        return false;
+      }
+    }    
+    else{      
+      if(user.type != "user") 
+        return false;
+
+      else 
+       return true;
     }
-    else{
-      console.log(user);
-    }
 
-  }
-}*/
-
-export const verifyUserToken = async (token: string, cache:any, manager: any) => { 
-  let user =  cache.get(token);
-
-  if ( user == undefined ){
-    console.log("undefined");
-  }
-  else{
-    console.log(user);
   }
 }
-
-
 
